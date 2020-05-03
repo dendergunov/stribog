5ee0bd3a536c68ee9f88e9bb14298d489c2aa8ac
Denis Dergunov <dan.dergunow@gmail.com>
2020-05-03 18:43:53 +0300

    Stribog code added.

--- stribog.hpp
+++ stribog.hpp
@@ -1,4 +1,10 @@
 #ifndef STRIBOG_HPP
 #define STRIBOG_HPP
 
-#endif // STRIBOG_HPP
+//permutation
+
+//hash512()
+void hash_512(const unsigned char* message, unsigned long long length, unsigned char out[512]);
+void hash_256(const unsigned char* message, unsigned long long length, unsigned char out[256]);
+
+#endif // STRIBOG_H
