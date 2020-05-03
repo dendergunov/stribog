ebbfa9fadb11dac8c5e87d64471f0c3630e6e689
Denis Dergunov <dan.dergunow@gmail.com>
2020-05-03 20:24:58 +0300

    Arity is now taken as a parameter instead of 3
    File generation with random bytes funciton is removed

--- main.cpp
+++ main.cpp
@@ -7,30 +7,36 @@
 #include <array>
 #include <string>
 #include <stdexcept>
-#include <random>
+#include <charconv>
+#include <optional>
 
 namespace {
-    int uniform_random(int min, int max){
-        static std::mt19937 prng(std::random_device{}());
-        return std::uniform_int_distribution<>(min, max)(prng);
-    }
+template<typename T>
+std::optional<T> from_chars(std::string_view sv_) noexcept
+{
+    T out;
+    auto end = sv_.data() + sv_.size();
+    auto res = std::from_chars(sv_.data(), end, out);
+    if(res.ec==std::errc{}&&res.ptr==end)
+        return out;
+    return {};
+}
 }
 
 int main(int argc, char **argv)
 {
     if (argc < 3)
-        throw std::runtime_error("Usage: <program-name> <arity> <file-name>");
+        throw std::runtime_error("Usage: <program-name> <arity> <file-name>\n");
+
+    auto l_arity = from_chars<std::size_t>(argv[1]);
+    if(!l_arity || *l_arity<2){
+        throw std::runtime_error("set arity >= 2\n");
+    }
 
     std::fstream chars_file(argv[2], std::ios::binary | std::ios::in | std::ios::out);
     if(!chars_file.is_open()){
-        throw std::runtime_error("Cannot open file!");
+        throw std::runtime_error("Cannot open file!\n");
     }
-
-    /*
-    for (int i = 0; i < (block_size*larity*15)+20; ++i){
-        chars_file << ((unsigned char) uniform_random(0, 255));
-    }
-    */
 
     std::vector<unsigned char> v{};
     std::stringstream ss;
@@ -39,14 +45,14 @@ int main(int argc, char **argv)
     v.insert(v.end(), s.begin(), s.end());
 
     std::cout << "File size in bytes: " << v.size() << ", in bits: " << v.size() * 8 << '\n';
-    std::cout << "Vector contains next bytes: \n";
+    /*std::cout << "Vector contains next bytes: \n";
     for(auto x: v){
         ft::hexcout{} << x;
         std::cout << ' ';
     }
     std::cout << "\n------------------------------------------\n";
-
-    ft::fts ft_stribog(std::move(v), 3);
+    */
+    ft::fts ft_stribog(std::move(v), *l_arity);
     v = ft_stribog.ft();
 
     std::cout << "\nFINALLY I'M DONE PLEASE GIVE ME A REST\n";
